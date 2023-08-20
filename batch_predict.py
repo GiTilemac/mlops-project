6a39e02dbdbf922eaf3ed11befc4a4a2bfab0302
Tilemachos Doganis <dtilemac@hotmail.com>
2023-08-20 11:03:07 +0200

    Expand batch_predict for monitoring flow

--- batch_predict.py
+++ batch_predict.py
@@ -8,6 +8,12 @@ from prefect import task, flow, get_run_logger
 from prefect.context import get_run_context
 from prefect_aws import S3Bucket
 
+from evidently.report import Report
+from evidently import ColumnMapping
+from evidently.metrics import ColumnDriftMetric, DatasetDriftMetric, DatasetMissingValuesMetric
+
+import psycopg2
+
 def read_dataframe(filename: str) -> pd.DataFrame:
     """ Read dataset into dataframe """
 
@@ -47,6 +53,77 @@ def save_results(df, y_pred, run_id, output_file):
     df_result['model_version'] = run_id
 
     df_result.to_csv(output_file, index=False)
+
+@task
+def prep_db(create_table_statement: str) -> psycopg2.extensions.cursor:
+    with psycopg2.connect("host=localhost port=5432 user=postgres password=example", autocommit=True) as conn:
+        res = conn.execute("SELECT 1 FROM pg_database WHERE datname='test'")
+        if len(res.fetchall()) == 0:
+            conn.execute("create database test;")
+        with psycopg2.connect("host=localhost port=5432 dbname=test user=postgres password=example") as conn:
+            conn.execute(create_table_statement)
+    return conn
+
+@task(log_prints=True)
+def generate_report(
+    training_data: pd.DataFrame,
+    validation_data: pd.DataFrame,
+    conn: psycopg2.extensions.cursor
+):
+    categorical = ['M']
+    numerical = ['age','creatinine', 'LYVE1', 'REG1B', 'TFF1']
+    target = ['diagnosis']
+
+    # Evidently Report
+    column_mapping = ColumnMapping(
+        target=None, # we're not analyzing the diagnosis variable
+        prediction=target[0],
+        numerical_features=numerical,
+        categorical_features=categorical
+    )
+
+    report = Report(metrics=[
+        ColumnDriftMetric(column_name=target[0]), # Prediction Drift
+        DatasetDriftMetric(),
+        DatasetMissingValuesMetric()
+    ]
+    )
+
+    report.run(reference_data=training_data, current_data=validation_data, column_mapping=column_mapping)
+    report_dict = report.as_dict()
+
+    prediction_drift = report_dict['metrics'][0]['result']['drift_score']
+    num_drifted_columns = report_dict['metrics'][1]['result']['number_of_drifted_columns']
+    share_missing_values = report_dict['metrics'][2]['result']['current']['share_of_missing_values']
+
+    conn.execute(
+        "insert into dummy_metrics(cohort, prediction_drift, num_drifted_columns, share_missing_values) values (%s, %s, %s, %s)",
+        ("cohort2", prediction_drift, num_drifted_columns, share_missing_values)
+    )
+
+@flow
+def monitoring_flow(
+    train_path: str = "./data/Debernardi_2020_data.csv",
+    validation_path: str="./data/Debernardi_2020_data_cohort2.csv"
+) -> None:
+    """ Evidently Monitoring Report Pipeline """
+    df_train = read_dataframe(train_path)
+    df_val = read_dataframe(validation_path)
+
+    create_table_statement = """
+    drop table if exists monitoring_metrics;
+    create table monitoring_metrics(
+        cohort string,
+        prediction_drift float,
+        num_drifted_columns integer,
+        share_missing_values float
+    )
+    """
+
+    db_cursor = prep_db(create_table_statement)
+
+    generate_report(df_train, df_val, db_cursor)
+
 
 @task
 def apply_model(input_file, run_id, output_file):
